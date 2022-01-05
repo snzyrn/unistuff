b38b3fa9911526061b58748116d0a3ca307a4b46
Yaren Şenöz <73521064+snzyrn@users.noreply.github.com>
2022-01-05 21:11:42 +0300

    Update profile.dart

--- Flutter/lib/profile.dart
+++ Flutter/lib/profile.dart
@@ -1,99 +1,116 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'settings.dart';
+import 'edit.dart';
 
-class ProfilePage extends StatefulWidget {
+class ProfilePage extends StatelessWidget {
   @override
-   _ProfilePage createState() {
-    return _ProfilePage();
+   Widget build(BuildContext context){
+     return Column(
+       children: [
+         ProfilePic(),
+         SizedBox(height: 20),
+         Info(),
+         Menu(
+           text: "Profile Edit",
+           press: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => EditProfilePage()));
+           },
+         ),
+         Menu(
+           text: "Settings",
+           press: () {
+             Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => SettingsPage()));
+           },
+         ),
+         Menu(
+           text: "Help Center",
+           press: () {},
+         ),
+          Menu(
+           text: "Log Out",
+           press: () {},
+         ),
+       ] 
+     );
   }
 }
 
-class _ProfilePage extends State<ProfilePage> {
-  bool showPassword = false;
-
-  get boxshape => null;
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-        backgroundColor: Colors.white54,
-        titleTextStyle: const TextStyle(fontSize: 30.0, color: Colors.white ),
-        elevation: 1,        
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.settings,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => SettingsPage()));
-            },
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: ListView(
-            children: [              
-              SizedBox(
-                height: 15,
-              ),
-              Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(                    
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: NetworkImage(
-                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs5Azv8erp-FKyJyj0Bs-pfviq0UlMXiWKcg&usqp=CAU'),
-                        ),
-                      ),
-                    ),                    
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              Text("Mary Louis"),
-              Text("MaryLouis@gmail.com"),
-              Text("Trabzon,Turkey"),             
-            ],
-          ),
-        ),
-      ),
+    return SizedBox(
+     height: 150,
+     width: 150,
+     child: CircleAvatar(backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs5Azv8erp-FKyJyj0Bs-pfviq0UlMXiWKcg&usqp=CAU'),
+       radius: 50,
+       ),
     );
   }
+}
 
-  Padding buildTextField(String labelText, String placeholder,
-      bool isPasswordTextField) {
+class Menu extends StatelessWidget {
+  const Menu({
+    Key? key,
+    required this.press,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback press;
+  @override
+  Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 35.0),
-      child: TextField(
-        obscureText: isPasswordTextField ? showPassword : false,
-        decoration: InputDecoration(
-          labelText: labelText,
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          hintText: placeholder,
-          hintStyle: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey,
-          ),
+      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+      child: FlatButton(
+        padding: EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        onPressed: press,
+        child: Row(
+          children: [
+            Icon(Icons.circle),
+            SizedBox(width: 30,),
+            Expanded(child: Text(text,style: Theme.of(context).textTheme.bodyText1,)),
+            Icon(Icons.arrow_forward_ios),
+          ],
         ),
-      ),
+        ),
+      );
+  }
+}
+
+class Info extends StatelessWidget{
+  const Info({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(), //             <--- BoxDecoration here
+            child: Text(
+            "User Name",
+            style: TextStyle(fontSize: 30.0),
+    ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(0.0),
+            padding: const EdgeInsets.all(0.0),
+            decoration: BoxDecoration(), //             <--- BoxDecoration here
+            child: Text(
+            "City",
+            style: TextStyle(fontSize: 20.0),
+          ),),
+        ],
+      )
     );
   }
 }
